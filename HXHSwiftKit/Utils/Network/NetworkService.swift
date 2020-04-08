ba4399cec110f77f3a9ae9c75b71013144d9601b
先至科技 <huangy@shidegroup.com>
2020-04-08 18:13:26 +0800

    tmp：网络

---
+++ HXHSwiftKit/Utils/Network/NetworkService.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkService.swift
+//  HXHSwiftKit
+//
+//  Created by 黄小豪 on 2020/4/8.
+//  Copyright © 2020 黄小豪. All rights reserved.
+//
+
+import UIKit
+class NetworkService: NSObject {
+
+}
+enum NetworkService {
+    case login
+    case goods
+    case order
+    case wallet
+}
+
